4d82656dc3435c00615282c567b0b0b90b8ce6e9
Mr.Anderson <a01769367@itesm.mx>
2023-05-23 14:37:54 -0600

    ProductView 1.0
    Product view adapted to figma design.

--- pBay/buyers/urls.py
+++ pBay/buyers/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("pedidos/", views.pedidos, name="pedidos")
+    path("pedidos/", views.pedidos, name="pedidos"),
+    path("productos/", views.productos, name="productos"),
 ]
--- pBay/buyers/views.py
+++ pBay/buyers/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def pedidos(request):
     return render(request, "pedidos.html")
 
+def productos(request):
+    return render(request, "productos.html")
+
