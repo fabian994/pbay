93e99ec4bb04afee4cf3649495063b09804bf673
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2023-05-23 11:56:31 -0600

    historial ventas y detalle historial pagos

--- pBay/sellers/urls.py
+++ pBay/sellers/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('mis_ventas/historial_ventas',
+         views.historial_ventas, name='historial_ventas'),
+    path('mis_ventas/historial_pagos/detalle',
+         views.historial_pagos_detalle, name='historial_pagos')
 ]
--- pBay/sellers/views.py
+++ pBay/sellers/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def historial_ventas(request):
+    return render(request, "historial_ventas.html")
+
+
+def historial_pagos_detalle(request):
+    return render(request, "historial_pagos_detalle.html")
