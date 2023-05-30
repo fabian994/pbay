1c4785079b76757ebfc4a7b2621aa7d8f6a0a54c
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2023-05-30 12:35:19 -0600

    Historial de ventas

--- pBay/sellers/views.py
+++ pBay/sellers/views.py
@@ -1,3 +1,4 @@
+from utils import sellsHistory
 from django.shortcuts import render
 from utils import infoventas
 from .form import *
@@ -5,44 +6,47 @@ from .form import *
 
 
 def historial_ventas(request):
-    return render(request, "historial_ventas.html")
+    user = request.session.get("usuario")
+    context = {"sells": sellsHistory(user.get("localId"))}
+    return render(request, "historial_ventas.html", context)
 
 
 def historial_pagos_detalle(request):
     return render(request, "historial_pagos_detalle.html")
 
+
 def detalles_producto(request):
     response = []
-    
+
     sesion = request.session['usuario']
-    
+
     if request.method == 'POST':
-        response =[]
+        response = []
         form = Filter(request.POST)
         if form.is_valid():
             selected_option2 = form.cleaned_data['Filtering']
             if selected_option2 == 'nada':
-               response = infoventas(sesion, 0)
+                response = infoventas(sesion, 0)
             elif selected_option2 == 'subasta':
-               response = infoventas(sesion, 1) 
+                response = infoventas(sesion, 1)
             else:
                 response = infoventas(sesion, 2)
     else:
-        response =infoventas(sesion, 0)
+        response = infoventas(sesion, 0)
         form = Filter()
-    context ={"htmlinfo":  response}
+    context = {"htmlinfo":  response}
     context['form'] = form
-    
-    return render(request, "Product_Details_Seller.html", context)
 
+    return render(request, "Product_Details_Seller.html", context)
 
 
 def historial_pagos(request):
     return render(request, "Payment_Details_Seller.html")
 
+
 def subastas(request):
     return render(request, "subastas.html")
 
+
 def add_product(request):
     return render(request, "add_product.html")
-
