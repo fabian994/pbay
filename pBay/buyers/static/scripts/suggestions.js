4e041bec344d98f475b5362f71722c56319cedfb
Marino <35277555+Mahrex308@users.noreply.github.com>
2023-06-08 19:43:07 -0600

    Correcion de css en productos.html dentro de la hoja de estilo de products.css

--- pBay/buyers/static/scripts/suggestions.js
+++ pBay/buyers/static/scripts/suggestions.js
@@ -1,10 +1,10 @@
 let suggestions = [
-  "Coco Fresco",
-  "Funda Molan Cano",
   "Cat toy",
-  "Street Fighter 6 Ultimate Edition",
-  "Los Juegos del Hambre",
-  "24h Primer",
-  "Aceite de Girasol",
   "ashsh",
+  "Aceite de Girasol",
+  "Los Juegos del Hambre",
+  "Funda Molan Cano",
+  "Coco Fresco",
+  "Street Fighter 6 Ultimate Edition",
+  "24h Primer",
 ];
