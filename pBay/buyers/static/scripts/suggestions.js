d950f2746319ad0f0541031fd69373904eca22c7
Ana <anarj14@hotmail.com>
2023-06-08 20:41:45 -0500

    fix delete button shopping cart

--- pBay/buyers/static/scripts/suggestions.js
+++ pBay/buyers/static/scripts/suggestions.js
@@ -1,10 +1,10 @@
 let suggestions = [
   "Coco Fresco",
-  "Funda Molan Cano",
-  "Cat toy",
-  "Street Fighter 6 Ultimate Edition",
-  "Los Juegos del Hambre",
-  "24h Primer",
-  "Aceite de Girasol",
   "ashsh",
+  "Street Fighter 6 Ultimate Edition",
+  "24h Primer",
+  "Cat toy",
+  "Aceite de Girasol",
+  "Funda Molan Cano",
+  "Los Juegos del Hambre",
 ];
